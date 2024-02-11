d54e968f00caa7d095bedbe09e784aae1c2108cc
EvaIlievska <ilievska_eva@yahoo.com>
2024-02-11 09:08:41 +0100

    vezba13

---
+++ src/proektt_3/MojaKlasa.java
@@ -0,0 +1,12 @@
+package proektt_3;
+ 
+import java.util.Scanner;
+
+public class MojaKlasa {
+
+	public static void main(String[] args) {
+		try (Scanner myVar = new Scanner(System.in)) {
+			System.out.print(myVar.nextLine());
+		}
+		}
+}
